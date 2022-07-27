23f3fb29675090a1e3dca3e7ca3a68e79508f670
Alex_ERA <eraspb@mail.ru>
2022-07-27 23:24:59 +0300

    New app Accounts_shop

--- shop_m/shop_m/settings.py
+++ shop_m/shop_m/settings.py
@@ -26,7 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'accounts_shop',
     'mobiles'
 ]
 
--- shop_m/shop_m/urls.py
+++ shop_m/shop_m/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mobiles.urls')),
+    path('', include('accounts_shop.urls')),
     path('', include('django.contrib.auth.urls')) # добавляем авторизацию по ссылке accounts
 
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
