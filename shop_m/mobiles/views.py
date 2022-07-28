371e2e77fed4375aadd6cf915a8801812909f2dc
Alex_ERA <eraspb@mail.ru>
2022-07-29 00:52:41 +0300

    new2

--- shop_m/mobiles/views.py
+++ shop_m/mobiles/views.py
@@ -33,7 +33,7 @@ def test(request):
     gl_img_verj = main_images.objects.filter(position1=True)
     gl_img_niz = main_images.objects.filter(position2=True)
 
-    top_model_im1 = Top_Models.objects.filter(show_art1=True)
+    top_model_im1 = Top_Models.object.filter(show_art1=True)
     return render(request, 'test.html', {'image_top': gl_img_verj, 'image_bot': gl_img_niz, 'top_model_image1' : top_model_im1})
 
 
